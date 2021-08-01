0e41a828d98a220f0292a8751141ada689060c93
moradalkhatib <muradalkhateb@icloud.com>
2021-08-01 20:48:51 +0300

    react

---
+++ src/component/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class Footer extends Component {
+    render() {
+        return (
+            <div>
+               <h5>The Author Is Murad Alkhatib</h5> 
+            </div>
+        )
+    }
+}
+
+export default Footer
