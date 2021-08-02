85093d5cd46e3ed10dccd13c26799af02fbd1015
moradalkhatib <muradalkhateb@icloud.com>
2021-08-02 15:01:42 +0300

    last editing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react'
-import Footer from './component/Footer'
-import Header from './component/Header'
-import Main from './component/Main'
-import './App.css'
+import React, { Component } from "react";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
+import Main from "./component/Main";
+import "./App.css";
 
 export class App extends Component {
   render() {
     return (
       <div>
-        <Header/>
-        <Main/>
-        <Footer/>
-
+        <Header />
+        <Main />
+        <Footer />
       </div>
-    )
+    );
   }
 }
 
-export default App
-
-
-
+export default App;
 
 //fixed main and add eslintrc
--- src/component/HornedBeasts.js
+++ src/component/HornedBeasts.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
- class HornedBeasts extends Component {
-    render() {
-        return (
-            <div className='counter'>
-               <h2>{this.props.best.title}</h2>
-               <img src={this.props.best.image_url} alt='' title=''/>
-               <p>{this.props.best.description}</p>
-               
-            </div>
-            
-        )
-    }
-    
+class HornedBeasts extends Component {
+  render() {
+    return (
+      <div className="counter">
+        <h2>{this.props.best.title}</h2>
+        <img src={this.props.best.image_url} alt="" title="" />
+        <p>{this.props.best.description}</p>
+      </div>
+    );
+  }
 }
 
-export default HornedBeasts
+export default HornedBeasts;
